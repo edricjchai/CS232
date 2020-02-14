8cae6d4c7f6771ff109dc69c29fc6295ff24c7e1
Edric Chai <ec73829n@pace.edu>
2020-02-14 04:43:23 -0500

    Completed task 3, added input handler

--- lab3/task3/wordstats.c
+++ lab3/task3/wordstats.c
@@ -5,16 +5,24 @@
 int main () {
 
   /* Zero out the array */
-  int letter_frequency[26] = {0};
-  int len, i;
-  char buf[MAX_BUF];
+      int letter_frequency[26] = {0};
+      int len, i;
+      char buf[MAX_BUF];
 
-  do {
-  } while (len > 1);
+      fgets(buf,MAX_BUF,stdin);
+      len = strlen(buf);
 
-  printf("Distribution of letters in corpus:\n");
-  for (i = 0; i < 26; i++) {
-    // 'A' is ASCII code 65
-    printf("%c: %d\n", 65+i, letter_frequency[i]);
-  }
+      do{
+            if (buf[len] >= 'A' && buf[len] <= 'Z')
+                letter_frequency[buf[len]-'A']++;
+            else if(buf[len] >= 'a' && buf[len] <= 'z')
+                letter_frequency[buf[len]-'a']++;
+            len--;
+      } while (len >= 0);
+
+      printf("Distribution of letters in corpus:\n");
+      for (i = 0; i < 26; i++) {
+        // 'A' is ASCII code 65
+        printf("%c: %d\n", 65+i, letter_frequency[i]);
+      }
 }
