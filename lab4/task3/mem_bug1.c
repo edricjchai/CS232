0d21e2180b8ea17e556d29a547015a1057089986
Edric Chai <ec73829n@pace.edu>
2020-02-29 17:03:39 -0500

    Freed malloc and calloc assignments, no memory lost

--- lab4/task3/mem_bug1.c
+++ lab4/task3/mem_bug1.c
@@ -9,11 +9,12 @@ int main(int argc, char * argv[]){
   *a = 10;  
 
   printf("%d\n", *a);
-
+  free(a);
   a = calloc(3, sizeof(int *));
   a[0] = 10;
   a[1] = 20;
   a[2] = 30;
-
+  
   printf("%d %d %d\n", a[0], a[1], a[2]);
+  free(a);
 }
